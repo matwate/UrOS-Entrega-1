89438f0da64528610ae5288ff8cd1cac1ec444c9
Matwa <mateowatata@gmail.com>
2026-02-11 16:36:02 -0500

    Added SFJ_NP ??

--- src/ur_os/SJF_NP.java
+++ src/ur_os/SJF_NP.java
@@ -5,28 +5,34 @@
 package ur_os;
 
 /**
- *
  * @author prestamour
  */
-public class SJF_NP extends Scheduler{
+public class SJF_NP extends Scheduler {
 
-    
-    SJF_NP(OS os){
-        super(os);
-    }
-    
-   
-    @Override
-    public void getNext(boolean cpuEmpty) {
-        
-       //Insert code here
-        
-    }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive
+  SJF_NP(OS os) {
+    super(os);
+  }
 
-    @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive
-    
+  @Override
+  public void getNext(boolean cpuEmpty) {
+    if (cpuEmpty && !processes.isEmpty()) {
+      Process shortest = processes.get(0);
+      for (Process p : processes) {
+        if (p.getBurstTime() < shortest.getBurstTime()
+            || (p.getBurstTime() == shortest.getBurstTime() && tieBreaker(p, shortest) == p)) {
+          shortest = p;
+        }
+      }
+      // Basicamente Encontrar el proceso con el menor tiempo de burst  para continuar;
+      os.cpu.addProcess(shortest);
+      processes.remove(shortest);
+      addContextSwitch();
+    }
+  }
+
+  @Override
+  public void newProcess(boolean cpuEmpty) {} // Non-preemtive
+
+  @Override
+  public void IOReturningProcess(boolean cpuEmpty) {} // Non-preemtive
 }
