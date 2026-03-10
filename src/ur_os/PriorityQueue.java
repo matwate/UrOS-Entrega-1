ddbc5393b4c4343f6edf8113b3a11290f1dbc28f
Matwa <mateowatata@gmail.com>
2026-03-09 22:11:50 -0500

    PriorityQueueScheduler Completed

--- src/ur_os/PriorityQueue.java
+++ src/ur_os/PriorityQueue.java
@@ -7,57 +7,62 @@ package ur_os;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-
 /**
- *
  * @author prestamour
  */
-public class PriorityQueue extends Scheduler{
+public class PriorityQueue extends Scheduler {
 
-    int currentScheduler;
-    
-    private ArrayList<Scheduler> schedulers;
-    
-    PriorityQueue(OS os){
-        super(os);
-        currentScheduler = -1;
-        schedulers = new ArrayList();
-    }
-    
-    PriorityQueue(OS os, Scheduler... s){ //Received multiple arrays
-        this(os);
-        schedulers.addAll(Arrays.asList(s));
-        if(s.length > 0)
-            currentScheduler = 0;
-    }
-    
-    
-    @Override
-    public void addProcess(Process p){
-       //Overwriting the parent's addProcess(Process p) method may be necessary in order to decide what to do with process coming from the CPU.
-       //On which queue should the process go?
-        
-    }
-    
-    void defineCurrentScheduler(){
-        //This methos is suggested to help you find the scheduler that should be the next in line to provide processes... perhaps the one with process in the queue?
-    }
-    
-   
-    @Override
-    public void getNext(boolean cpuEmpty) {
-        //Suggestion: now that you know on which scheduler a process is, you need to keep advancing that scheduler. If it a preemptive one, you need to notice the changes
-        //that it may have caused and verify if the change is coherent with the priority policy for the queues.
-        //Suggestion: if the CPU is empty, just find the next scheduler based on the order and the existence of processes
-        //if the CPU is not empty, you need to define that will happen with the process... if it fully preemptive, and there are process pending in higher queue, does the
-        //scheduler removes a process from the CPU or does it let it finish its quantum? Make this decision and justify it.
-  
-    }
-    
-    @Override
-    public void newProcess(boolean cpuEmpty) {} //Non-preemtive in this event
+  int currentScheduler;
 
-    @Override
-    public void IOReturningProcess(boolean cpuEmpty) {} //Non-preemtive in this event
-    
+  private ArrayList<Scheduler> schedulers;
+
+  PriorityQueue(OS os) {
+    super(os);
+    currentScheduler = -1;
+    schedulers = new ArrayList();
+  }
+
+  PriorityQueue(OS os, Scheduler... s) { // Received multiple arrays
+    this(os);
+    schedulers.addAll(Arrays.asList(s));
+    if (s.length > 0) currentScheduler = 0;
+  }
+
+  @Override
+  public void addProcess(Process p) {
+    int queueIndex = p.getPriority();
+    if (queueIndex < 0) queueIndex = 0;
+    if (queueIndex >= schedulers.size()) queueIndex = schedulers.size() - 1;
+    p.setCurrentScheduler(queueIndex);
+    schedulers.get(queueIndex).addProcess(p);
+  }
+
+  void defineCurrentScheduler() {
+    int current = -1;
+
+    for (int i = 0; i < schedulers.size(); i++) {
+      if (!schedulers.get(i).isEmpty()) {
+        current = i;
+        break;
+      }
+    }
+    currentScheduler = current;
+  }
+
+  @Override
+  public void getNext(boolean cpuEmpty) {
+    if (cpuEmpty) {
+      defineCurrentScheduler();
+      if (currentScheduler != -1) {
+        schedulers.get(currentScheduler).getNext(true);
+      }
+      return;
+    }
+  }
+
+  @Override
+  public void newProcess(boolean cpuEmpty) {} // Non-preemtive in this event
+
+  @Override
+  public void IOReturningProcess(boolean cpuEmpty) {} // Non-preemtive in this event
 }
